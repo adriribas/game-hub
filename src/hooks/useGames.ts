7fe49a7ffbf42876d27913e3ef593111a305592d
Adrià Ribas <ribasadria00@gmail.com>
2023-05-04 12:14:24 +0200

    Build platform selector

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,7 +13,6 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
-  genres: Genre[];
 }
 
 const useGames = (selectedGenre: Genre | null) =>
