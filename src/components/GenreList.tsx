48156ba484649e9f96cbd3102fbc0c8570a9c6fd
Adrià Ribas <ribasadria00@gmail.com>
2023-05-08 23:19:23 +0200

    Create a reusable API client

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,7 +1,6 @@
 import { Button, Heading, Image, List, ListItem } from '@chakra-ui/react';
 import useGenres, { Genre } from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
-import GenreSkeleton from './GenreSkeleton';
 import GenreContainer from './GenreContainer';
 
 interface Props {
@@ -10,10 +9,7 @@ interface Props {
 }
 
 const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
-  const { data, isLoading, error } = useGenres();
-  const skeletons = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
-  ];
+  const { data, error } = useGenres();
 
   if (error) return null;
 
@@ -23,12 +19,6 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
         Genres
       </Heading>
       <List>
-        {isLoading &&
-          skeletons.map(skeleton => (
-            <GenreContainer key={skeleton}>
-              <GenreSkeleton />
-            </GenreContainer>
-          ))}
         {data?.results.map(genre => (
           <ListItem key={genre.id}>
             {
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,22 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
     key: '89ee7de1ecaf4e3e9ec9d01e66792b7d'
   }
 });
+
+export default class APIClient<T> {
+  constructor(private endpoint: string) {}
+
+  getAll = (config: AxiosRequestConfig) =>
+    axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then(res => res.data);
+}
